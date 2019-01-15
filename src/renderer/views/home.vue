ceb1647f33d9083e3c9152c9bd890c76dd960579
jk4235 <835633812@qq.com>
2019-01-16 01:48:28 +0800

    home页 20%

--- src/renderer/views/home.vue
+++ src/renderer/views/home.vue
@@ -1,10 +1,31 @@
 <template>
     <el-container>
-        <el-header>test</el-header>
-        <el-main>
-            <ul>
-                <li v-for="link in links">{{ link }}</li>
-            </ul>
+        <el-header style="height: 120px; width: 485px">
+            <div class="typeSelect">
+                <el-tag
+                    v-for="(v, k) in types"
+                    @click.native="toggleSelect(k)"
+                    :type="v ? 'default' : 'info'">{{ k }}</el-tag>
+            </div>
+            <el-row type="flex" justify="space-between">
+                <el-col :span="17">
+                    <div>
+                        <el-input placeholder="Search Keywords"></el-input>
+                    </div>
+                </el-col>
+                <el-col :span="6">
+                    <div style="display: flex; justify-content: space-between">
+                        <el-button class="searchBtn">搜索</el-button>
+                        <el-button class="searchBtn">清除</el-button>
+                    </div>
+                </el-col>
+            </el-row>
+        </el-header>
+        <el-main style="padding-top: 0">
+            <el-card shadow="hover" v-for="book in books">
+                <div class="cover"><img :src="book.cover"></div>
+                <a :href="book.detailLink">{{ book.detailLink }}</a>
+            </el-card>
         </el-main>
     </el-container>
 </template>
@@ -17,7 +38,19 @@ export default {
   name: 'home',
   data () {
     return {
-      links: []
+      types: {
+        'DOUJINSHI': 1,
+        'MANGA': 1,
+        'ARTIST CG': 0,
+        'GAME CS': 0,
+        'WESTERN': 0,
+        'NON-H': 0,
+        'IMAGE SET': 0,
+        'COSPLAY': 0,
+        'ASIAN PORN': 0,
+        'MISC': 0
+      },
+      books: []
     }
   },
   methods: {
@@ -28,13 +61,26 @@ export default {
     },
     parseHtml (html) {
       const $ = cheerio.load(html)
-      const reNextLink = /^https:\/\/e-hentai.org\/g\//
-      $('tr').find('a').each((index, cv) => {
-        const link = cv.attribs.href
-        if (reNextLink.test(link)) {
-          this.links.push(link)
+      $('tr[class="gtr0"], tr[class="gtr1"]').each((index, cv) => {
+        const detailLink = $(cv).find('.it5 > a').prop('href')
+        let cover = null
+        if ($(cv).find('.it2').find('img').length) {
+          cover = $(cv).find('.it2').find('img').prop('src')
+        } else {
+          cover = this.getCover($(cv).find('.it2').text())
         }
+        this.books.push({
+          detailLink,
+          cover
+        })
       })
+    },
+    toggleSelect (k) {
+      this.types[k] = this.types[k] ? 0 : 1
+    },
+    getCover (str) {
+      const a = str.split('~', 4)
+      return a[0] === 'init' ? `http://${a[1]}/${a[2]}` : `https://${a[1]}/${a[2]}`
     }
   },
   mounted () {
@@ -44,5 +90,27 @@ export default {
 </script>
 
 <style scoped>
-
+.typeSelect{
+    margin: auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    align-content: space-around;
+    height: 75px;
+}
+.searchBtn{
+    padding: 10px;
+}
+.cover{
+    border:2px solid;
+    height:133px;
+    width:100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+}
+.cover > img {
+    width: 100%;
+    max-height: 100%;
+}
 </style>
